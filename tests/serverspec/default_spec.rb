dd9734642e6e5c563982c968ed8275ad9111540e
Tomoyuki Sakurai <y@trombik.org>
2019-10-02 13:17:51 +0900

    bugfix: test sensu_backend_extra_packages

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -6,24 +6,33 @@ service = "sensu-backend"
 config  = "/etc/sensu/backend.yml"
 user    = "sensu"
 group   = "sensu"
-ports   = []
+ports   = [3000, 8080, 8081]
 log_dir = "/var/log/sensu"
 db_dir  = "/var/lib/sensu"
 cache_dir = "/var/cache/sensu/sensu-backend"
+extra_packages = %w{sensu-go-cli}
 
 case os[:family]
 when "freebsd"
   config = "/usr/local/etc/sensu_backend.conf"
   db_dir = "/var/db/sensu_backend"
+  extra_packages = []
 end
 
 describe package(package) do
   it { should be_installed }
 end
 
+extra_packages.each do |p|
+  describe package p do
+    it { should be_installed }
+  end
+end
+
 describe file(config) do
   it { should be_file }
   its(:content) { should match Regexp.escape("Managed by ansible") }
+  its(:content) { should match(/log-level: info/) }
 end
 
 describe file(log_dir) do
